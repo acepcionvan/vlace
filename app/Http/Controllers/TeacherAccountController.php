51b38e2fcf21c3ada48e8b51076215c14d255ea8
acepcionvan <acepcionvan@gmail.com>
2026-08-08 23:39:50 +0800

    Add admin teacher login setup

---
+++ app/Http/Controllers/TeacherAccountController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class TeacherAccountController extends Controller
+{
+    public function __invoke(): Response
+    {
+        abort_unless(Auth::user()?->role === 'admin', 403);
+
+        $teacher = User::updateOrCreate(
+            ['email' => 'teacher@vlace.com'],
+            [
+                'name' => 'Maria Santos',
+                'password' => Hash::make('prototype'),
+                'role' => 'teacher',
+                'email_verified_at' => now(),
+            ],
+        );
+
+        return response("Teacher login ready: {$teacher->email} / prototype", 200)
+            ->header('Content-Type', 'text/plain');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SetupController;
+use App\Http\Controllers\TeacherAccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/login', [AuthController::class, 'login'])->middleware('guest')->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 Route::get('/setup/{key}', SetupController::class)->name('setup');
+Route::get('/admin/create-teacher-login', TeacherAccountController::class)->middleware('auth')->name('teacher-login.setup');
 Route::post('/generate', [HomeController::class, 'generate'])->name('generate');
